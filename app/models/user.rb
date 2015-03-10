bffd9ba26191d551dc1d5969f705c01d66539460
Matt Moldavan <matt.moldavan@careerbuilder.com>
2015-03-10 00:41:32 -0400

    assets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User
   include Mongoid::Document
   store_in collection: 'users'
-  
+
   field :username, type: String
   field :email, type: String
   field :password, type: String
@@ -9,9 +9,18 @@ class User
   field :language, type: String
   field :country, type: String
 
+  index({ username: 1 }, { unique: true })
+
   validates_uniqueness_of :username
   validates_presence_of :email
   validates_presence_of :password
 
   validates :age, format: { with: /0-99/, allow_blank: true }
+
+
+  def before_save
+
+
+  end
+
 end
