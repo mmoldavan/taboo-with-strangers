73c647c9bf61dc6fcec159d05043cc096b16760e
Matt Moldavan <matt.moldavan@careerbuilder.com>
2015-03-09 23:56:59 -0400

    user model

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User
+  include Mongoid::Document
+  store_in collection: 'users'
+  
+  field :username, type: String
+  field :email, type: String
+  field :password, type: String
+  field :age, type: String
+  field :language, type: String
+  field :country, type: String
+
+  validates_uniqueness_of :username
+  validates_presence_of :email
+  validates_presence_of :password
+
+  validates :age, format: { with: /0-99/, allow_blank: true }
+end
